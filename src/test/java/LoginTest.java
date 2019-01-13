19231c97c8b091724a1fcffa0562c3dc5f168795
seveniruby <seveniruby@gmail.com>
2019-01-13 16:34:16 +0800

    增加数据驱动

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,5 @@
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import page.LoginPage;
 import page.MainPage;
 import page.ProfilePage;
@@ -7,12 +8,16 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LoginTest {
-    @Test
-    public void 非手机号(){
+    @ParameterizedTest
+    @CsvSource({
+            "15600534760, xxxxxxxx, 用户名或密码错误",
+            "1560053476, xxxxxxxx, 手机号码填写错误"
+    })
+    public void 密码登录(String username, String password, String expection){
         MainPage mainPage=MainPage.start();
         ProfilePage profilePage=mainPage.gotoProfile();
         LoginPage loginPage=profilePage.gotoLogin();
-        loginPage.passwordFail("15600534760", "xxxxxxxx");
-        assertThat(loginPage.getMessage(), equalTo("手机号码填写错误"));
+        loginPage.passwordFail(username, password);
+        assertThat(loginPage.getMessage(), equalTo(expection));
     }
 }
